1f3d4e3548894d36557dcabb05a8e9e6e324390f
charles Gift <charlesgiftangila@gmail.com>
2024-07-16 18:23:44 +0300

    Added click event to favourites button,added borders to games and styled the footer

--- game.js
+++ game.js
@@ -6,11 +6,28 @@ function renderOneGame(game) {
       <img src="${game.thumbnail}">
       <div class="content">
       <h4 id="title"> ${game.title} </h4>
+      <h5 id="desc"> Genre :${game.genre}</h5>
+      <p id="desc"> <span>Release Date:</span> ${game.release_date}</p>
       <p id="desc">${game.short_description}</p>
+      
+      <p id="desc"> Developer: ${game.developer}</p>
+      <button id="favourites">Add to Favourites</button>
+     
+      
       
       `;
   document.querySelector("#gameList").appendChild(card);
+  let btn = card.querySelector("#favourites");
+  function handleAddToFavouritesButton(e) {
+     btn = e.currentTarget;
+    btn.textContent = "Added to Favourites";
+    btn.disabled = true;
+    btn.style.color="red"
+    
+  }
+  btn.addEventListener("click", (handleAddToFavouritesButton))
 }
+
 function renderAllGames() {
   fetch("http://localhost:3000/games")
     .then((res) => res.json())
