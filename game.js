4986fb958036740b0c07f92d559ee856d7628031
charles Gift <charlesgiftangila@gmail.com>
2024-07-17 11:16:03 +0300

    Added mouse over,mouseout and click event listeners to card

--- game.js
+++ game.js
@@ -6,26 +6,52 @@ function renderOneGame(game) {
       <img src="${game.thumbnail}">
       <div class="content">
       <h4 id="title"> ${game.title} </h4>
+      <div id="detailsbox">
       <h5 id="desc"> Genre :${game.genre}</h5>
       <p id="desc"> <span>Release Date:</span> ${game.release_date}</p>
       <p id="desc">${game.short_description}</p>
       
       <p id="desc"> Developer: ${game.developer}</p>
       <button id="favourites">Add to Favourites</button>
+      </div>
      
       
       
       `;
+
   document.querySelector("#gameList").appendChild(card);
+  let titleGame = card.querySelector("#detailsbox")
+  function handleMouseOut(e) {
+    titleGame = e.currentTarget;
+    if (titleGame) {
+        titleGame.style.backgroundColor = "";
+    }  
+  }
+  
+  ;
+  function handleMouseOver(e) {
+    titleGame = e.currentTarget;
+    if (titleGame) {
+        titleGame.style.backgroundColor = "#1aeeef";
+        titleGame.style.transition = "2.5s";
+    }  
+  }
+  
+  
+  
+  
+  titleGame.addEventListener("mouseover", handleMouseOver);
+  titleGame.addEventListener("mouseout", handleMouseOut)
+
   let btn = card.querySelector("#favourites");
   function handleAddToFavouritesButton(e) {
-     btn = e.currentTarget;
+    btn = e.currentTarget;
     btn.textContent = "Added to Favourites";
     btn.disabled = true;
-    btn.style.color="red"
-    
+    btn.style.color = "red";
+    btn.disabled = false;
   }
-  btn.addEventListener("click", (handleAddToFavouritesButton))
+  btn.addEventListener("click", handleAddToFavouritesButton);
 }
 
 function renderAllGames() {
