84a34176d1b87df8cd61a81dfbe80e028d17f6fb
charles Gift <charlesgiftangila@gmail.com>
2024-07-23 12:27:03 +0300

    completed

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-const gamesUrl = "https://gamers-backend.vercel.app/games";
-function renderOneGame(game) {
-  let card = document.createElement("li");
-  card.className = "card";
-  card.innerHTML = `
+  const gamesUrl = "https://gamers-backend.vercel.app/games";
+  function renderOneGame(game) {
+    let card = document.createElement("li");
+    card.className = "card";
+    card.innerHTML = `
       <img src="${game.thumbnail}">
       <div class="content">
       <h4 id="title"> ${game.title} </h4>
@@ -20,45 +20,45 @@ function renderOneGame(game) {
       
       `;
 
-  document.querySelector("#gameList").appendChild(card);
-  let titleGame = card.querySelector("#detailsbox");
-  function handleMouseOut(e) {
-    titleGame = e.currentTarget;
-    if (titleGame) {
-      titleGame.style.backgroundColor = "";
+    document.querySelector("#gameList").appendChild(card);
+    let titleGame = card.querySelector("#detailsbox");
+    function handleMouseOut(e) {
+      titleGame = e.currentTarget;
+      if (titleGame) {
+        titleGame.style.backgroundColor = "";
+      }
     }
-  }
 
-  function handleMouseOver(e) {
-    titleGame = e.currentTarget;
-    if (titleGame) {
-      titleGame.style.backgroundColor = "#1aeeef";
-      titleGame.style.transition = "2.5s";
+    function handleMouseOver(e) {
+      titleGame = e.currentTarget;
+      if (titleGame) {
+        titleGame.style.backgroundColor = "#1aeeef";
+        titleGame.style.transition = "2.5s";
+      }
     }
+
+    titleGame.addEventListener("mouseover", handleMouseOver);
+    titleGame.addEventListener("mouseout", handleMouseOut);
+
+    let btn = card.querySelector("#favourites");
+    function handleAddToFavouritesButton(e) {
+      btn = e.currentTarget;
+      btn.textContent = "Added to Favourites";
+      btn.disabled = true;
+      btn.style.color = "red";
+      btn.disabled = false;
+    }
+    btn.addEventListener("click", handleAddToFavouritesButton);
   }
 
-  titleGame.addEventListener("mouseover", handleMouseOver);
-  titleGame.addEventListener("mouseout", handleMouseOut);
-
-  let btn = card.querySelector("#favourites");
-  function handleAddToFavouritesButton(e) {
-    btn = e.currentTarget;
-    btn.textContent = "Added to Favourites";
-    btn.disabled = true;
-    btn.style.color = "red";
-    btn.disabled = false;
+  function renderAllGames() {
+    fetch("https://gamers-backend.vercel.app/games")
+      .then((res) => res.json())
+      .then((gameData) => gameData.forEach((game) => renderOneGame(game)));
   }
-  btn.addEventListener("click", handleAddToFavouritesButton);
-}
+  function initialize() {
+    renderAllGames();
+  }
 
-function renderAllGames() {
-  fetch("https://gamers-backend.vercel.app/games")
-    .then((res) => res.json())
-    .then((gameData) => gameData.forEach((game) => renderOneGame(game)));
-}
-function initialize() {
-  renderAllGames();
-}
-
-initialize();
-})
+  initialize();
+});
